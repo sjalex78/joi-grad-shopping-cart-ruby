b1046cdaa3f09a67626469057165284d6db735c1
Larissa Marcich <lmarcich@thoughtworks.com>
2020-08-05 19:25:51 -0500

    [Larissa] Add additional test cases

--- spec/model/shopping_cart_spec.rb
+++ spec/model/shopping_cart_spec.rb
@@ -4,10 +4,45 @@ require './src/model/product'
 
 RSpec.describe ShoppingCart do
 
-  it "should validate information passed on to confirmation" do
+  it "should calculate correct price for item without discount" do
+    cart = ShoppingCart.new(Customer.new("test"), [] << Product.new(100, "no_discount_ABCD", "T"))
+    order = cart.checkout()
+
+    expect(order.total_price).to eq 100.0
+  end
+
+  it "should calculate correct loyalty points for item without discount" do
+    cart = ShoppingCart.new(Customer.new("test"), [] << Product.new(100, "no_discount_ABCD", "T"))
+    order = cart.checkout()
+
+    expect(order.loyalty_points).to eq 20
+  end
+
+  it "should calculate correct price for item with 10% discount" do
     cart = ShoppingCart.new(Customer.new("test"), [] << Product.new(100, "DIS_10_ABCD", "T"))
     order = cart.checkout()
 
     expect(order.total_price).to eq 90.0
+  end
+
+  it "should calculate correct loyalty points for item with 10% discount" do
+    cart = ShoppingCart.new(Customer.new("test"), [] << Product.new(100, "DIS_10_ABCD", "T"))
+    order = cart.checkout()
+
+    expect(order.loyalty_points).to eq 10
+  end
+
+  it "should calculate correct price for item with 15% discount" do
+    cart = ShoppingCart.new(Customer.new("test"), [] << Product.new(100, "DIS_15_ABCD", "T"))
+    order = cart.checkout()
+
+    expect(order.total_price).to eq 85.0
+  end
+
+  it "should calculate correct loyalty points for item with 15% discount" do
+    cart = ShoppingCart.new(Customer.new("test"), [] << Product.new(100, "DIS_15_ABCD", "T"))
+    order = cart.checkout()
+
+    expect(order.loyalty_points).to eq 6
   end
 end
